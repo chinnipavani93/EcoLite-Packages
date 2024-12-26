6777076e21177c3dd3836be5a7616856ca764e89
chinnipavani93 <chinnipavani.cse2021@citchennai.net>
2024-12-27 04:26:35 +0530

    changes made

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://eco-lite-packages.vercel.app/api/v1";
-
+const BASE_URL = "http://localhost:8000/api/v1";
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
+
